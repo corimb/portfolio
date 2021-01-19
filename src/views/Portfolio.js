689b4e9c27ef37e925e0aff4fbbe0b0d286a3435
Corina <cborcoci@gmail.com>
2021-01-19 23:15:47 +0100

    Add portfolio project examples

--- src/views/Portfolio.js
+++ src/views/Portfolio.js
@@ -1,8 +1,25 @@
 import React from 'react';
-
+import anonymousProxy from '../images/anonymous-proxy.png'
+import rickAndMorty from '../images/rick and morty.png'
 const Portfolio = () => {
     return(
-    <p>Esto es Portfolio</p>)
+    <section className="portfolio-section">
+        <article className="anonymousProxy-project">
+            <a href="https://github.com/Adalab/modulo-1-evaluacion-final-corimb" title="Go to Anonymous proxy project on GitHub" rel="noopener noreferrer" target="_blank" className="anonymous-proxy-url">
+            <span className="text">HTML & CSS</span>
+            <img src={anonymousProxy} alt="anonymous proxy project" className="anonymous-proxy-photo"></img>
+            </a>
+        </article>
+        <article className="rickAndMorty-project">
+            <a href="https://github.com/Adalab/modulo-3-evaluacion-final-corimb" title="Go to Rick and Morty project on GitHub"rel="noopener noreferrer" target="_blank" className="rickAndMorty-url">
+            <span className="text">React Js</span>
+            <img src={rickAndMorty} alt="Rick and Morty project" className="rickAndMorty-photo"></img>
+            </a>
+        </article>
+    </section>
+    
+    
+    )
 }
 
 export default Portfolio;
